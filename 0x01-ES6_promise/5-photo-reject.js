037105cd066e1c933c6677b09f1347245f76f7d8
Nnennia <nnenniaatuanya@gmail.com>
2023-04-21 00:06:39 +0100

    commitr

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,4 +1,4 @@
 /*eslint-disable */
 export default function uploadPhoto(filename) {
-    return Promise.reject(new Error(`${filename} can not be processed`));
+    return Promise.reject(new Error(`${filename} cannot be processed`));
 }
