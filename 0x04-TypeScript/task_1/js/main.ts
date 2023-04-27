7c694ca2d077b67f0ca88971ce3cfe2b649a39be
Nnennia <nnenniaatuanya@gmail.com>
2023-04-27 10:08:15 +0100

    commit

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -9,6 +9,41 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReport: number;
 }
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+function printTeacher(firstName: string, lastName: string) {
+  return `${firstName[0]}.${lastName}}`;
+}
+
+export interface IStudentClassConstructor {
+  new (firstName: string, lastName: string): IStudentClass;
+}
+
+export interface IStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements IStudentClass {
+  private _firstName!: string;
+  private _lastName!: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomework() {
+    return "Currently working";
+  }
+
+  displayName() {
+    return this._firstName;
+  }
+}
+
 const teacher3: Teacher = {
   firstName: "John",
   fullTimeEmployee: false,
